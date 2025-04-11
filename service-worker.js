f64655db948e416936d5707c1dc1d2bd69271b37
PapryczkQ12 <pstryczek41@gmail.com>
2025-04-11 18:24:07 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
-<script>
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(reg => console.log('✅ Zarejestrowano service worker:', reg.scope))
-        .catch(err => console.error('❌ Błąd SW:', err));
-    });
-  }
-</script>
+self.addEventListener("install", (event) => {
+  console.log("Service Worker zainstalowany.");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
